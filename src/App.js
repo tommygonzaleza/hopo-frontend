b6ebf48398e7647937d0ca7758d64bebd77baca6
tommygonzaleza <tommygonzaleza@users.noreply.github.com>
2020-03-26 23:08:18 -0400

    Form added.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import logo from './img/logo-white-horizontal.png';
+import MyForm from './components/MyForm';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <div className="main-navbar-styles">
+        <img className="main-logo-styles" src={logo} alt="Hopo Logo" />
+        
+      </div>
+      <MyForm />
     </div>
   );
 }
